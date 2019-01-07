1f8e77691e72ef94e6836bfa8a8f93fb9e5c1daa
Vladislav Shevchenko <vladprogramer@gmail.com>
2019-01-08 01:11:02 +0200

    ver 1

---
+++ server.py
@@ -0,0 +1,30 @@
+import socket, time
+
+clients = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', 9090))
+
+quit = False
+print('[ Server Started ]')
+
+while not quit:
+    try:
+        data, addr = s.recvfrom(1024)
+
+        if addr not in clients:
+            clients.append(addr)
+
+        itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+
+        print("[" + addr[0] + "]=[" + str(addr[1]) + "]=[" + itsatime + "]/", end="")
+        print(data.decode("utf-8"))
+
+        for client in clients:
+            if addr != client:
+                s.sendto(data, client)
+    except:
+        print("\n[ Server Stopped ]")
+        quit = True
+
+s.close()
